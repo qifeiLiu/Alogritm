98f8f0944c9150590d898d1197d4b00f6c509fd4
qifei.liu <qifei.liu@citrix.com>
2019-05-13 15:05:15 +0800

    stack example 1

--- LinkStack/main.c
+++ LinkStack/main.c
@@ -2,22 +2,119 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
-int main()
+int isLeft(char c)
 {
+	int ret = 0;
+	switch (c)
+	{
+	case '<':
+	case '(':
+	case '[':
+	case '{':
+	case '\'':
+	case '\"':
+		ret = 1;
+		break;
+	default:
+		ret = 0;
+		break;
+	}
+	return ret;
+}
 
-	int a[2] = { 1,2 };
+int isRight(char c)
+{
+	int ret = 0;
+	switch (c)
+	{
+	case '>':
+	case ')':
+	case ']':
+	case '}':
+	case '\'':
+	case '\"':
+		ret = 1;
+		break;
+	default:
+		ret = 0;
+		break;
+	}
+	return ret;
+}
+
+int match(char left, char right)
+{
+	int ret = 0;
+	switch (left)
+	{
+	case '<':
+		ret = (right == '>');
+		break;
+	case '(':
+		ret = (right == ')');
+		break;
+	case '[':
+		ret = (right == ']');
+		break;
+	case '{':
+		ret = (right == '}');
+		break;
+	case '\'':
+		ret = (right == '\'');
+		break;
+	case '\"':
+		ret = (right == '\"');
+		break;
+	default:
+		ret = 0;
+		break;
+	}
+	return ret;
+}
+
+int scanner(const char* code)
+{
 	LinkStack* stack = LinkStack_Create();
-	printf("Length:%d\n", LinkStack_Size(stack));
-	LinkStack_Push(stack, &a[0]);
-	printf("Length:%d\n", LinkStack_Size(stack));
-	LinkStack_Push(stack, &a[1]);
-	printf("Length:%d\n", LinkStack_Size(stack));
-	printf("Pop:%d\n", *(int*)LinkStack_Pop(stack));
-	printf("Length:%d\n", LinkStack_Size(stack));
-	printf("Pop:%d\n", *(int*)LinkStack_Pop(stack));
-	printf("Length:%d\n", LinkStack_Size(stack));
+	int ret = 0;
+	int i = 0;
+
+	while (code[i] != '\0')
+	{
+		if (isLeft(code[i]))
+		{
+			LinkStack_Push(stack, (void*)(code + i));
+		}
+		if (isRight(code[i]))
+		{
+			char* c = (char*)LinkStack_Pop(stack);
+			if ((c == NULL) || !match(*c, code[i]))
+			{
+				printf("%c does not match!\n",code[i]);
+				ret = 0;
+				break;
+			}
+		}
+		i++;
+	}
+	if ((LinkStack_Size(stack) == 0) && (code[i] == '\0'))
+	{
+		printf("Succeed!\n");
+		ret = 1;
+	}
+	else
+	{
+		printf("Invalid code!\n");
+		ret = 0;
+	}
+	LinkStack_Destory(stack);
+	return ret;
+}
+
+int main() 
+{
+	const char* code = "#include <stdio.h> int main() { int a[5][5]; int (*p)[4]; p = a[0]; printf(\"%d\\n\", &p[3][3] - &a[3][3]); return 0; }";
+
+	scanner(code);
 
 	system("pause");
 	return 0;
